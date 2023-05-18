ecbbb764d5108d81b77023c666cab1c68d217e99
Matheus Ned <matheusned15@gmail.com>
2023-05-17 23:40:51 -0300

    Token Authentication and Service Creation

---
+++ src/app/pages/login/login.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './login.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { LoginRotas } from './login.routes';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(LoginRotas)
+  ]
+})
+export class LoginModule { }
